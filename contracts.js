449e38b72abccf9b1d80a4fe9608ce66c44222b3
sssubik <subikshrestha@gmail.com>
2021-09-02 16:21:29 +0545

    creating deploy scripts

---
+++ contracts.js
@@ -0,0 +1,19 @@
+CONTRACTS= {
+        "rinkeby":{
+        "lotteryAddress": "0xBf910936d57673B5c3e3185578B4335B41fbb8F5",
+        "tokenAddress": "0x555eFBeC9ACd1054f53Cf80500a7aD134732f20F",
+        "nftAddress": "0xA83bBFfBFD4C7FAAA9CE4fe679E7900765DFC586",
+        "randomnessAddress": "0xc63343D411fd8ca6734b75E469D84B2Ec9f4159E",
+        "stakingAddress": "0x00986DAf2CcAa854f2e65fE1d536b0E2388c91e0"
+     },
+     "localhost":{
+        "lotteryAddress": "",
+        "tokenAddress": "",
+        "nftAddress": "",
+        "randomnessAddress": "0xc63343D411fd8ca6734b75E469D84B2Ec9f4159E",
+        "stakingAddress": ""
+    }
+    }
+
+
+module.exports = CONTRACTS 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,73 @@
 
 const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const CONTRACTS = require('../contracts.js')
+//TODO: CHECK HOW TO INITIALIZE Token Supply
+
+deploy_MemeXToken = async (deployer) => {
+  
+  const MemeToken = await hre.ethers.getContractFactory("MemeXToken");
+  const token = await MemeToken.deploy("MEMEX", "MemeX", 1000000, deployer.address);
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
+  return token
+}
+
+deploy_Staking = async (deployer,token) => {
+  const Staking = await hre.ethers.getContractFactory("MemeXStaking");
+  const stake = await Staking.deploy(token.address, deployer.address);
+  await stake.deployed();
+  console.log("Staking deployed to:", stake.address);
+  return stake
+}
+
+deploy_Lottery = async (deployer) => {
+  const Lottery = await hre.ethers.getContractFactory("Lottery");
+  const lottery = await Lottery.deploy(stake.address);
+  await lottery.deployed();
+  console.log("Lottery deployed to:", lottery.address);
+  return lottery
+}
+
+set_RandomGenerator = async (lottery,rng) => {
+  const Lottery = await ethers.getContractFactory("Lottery");
+  lottery = await Lottery.attach(lottery);
+  try{
+    lottery.setRandomGenerator(CONTRACTS[hre.network]["randomnessAddress"], {gasLimit: 4000000})
+  }catch (err) {
+    console.log(err);
+    return;
+  }
+  return lottery
+}
+
+deploy_Randomness = async () => {
+  
+  rand_address = CONTRACTS[hre.network.name]["randomnessAddress"]
+  const Randomness = await hre.ethers.getContractFactory("RandomNumberConsumer");
+  if (rand_address == ""){
+        _vrfCoordinator = "0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B"
+        _linkToken = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
+        _lotteryAddr = "0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9"
+        _keyHash = "0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311"
+        _fee = 1
+         randomness = await Randomness.deploy(_vrfCoordinator,
+          _linkToken,
+          _lotteryAddr,
+          _keyHash,
+          _fee)
+    }
+  else {
+     randomness =await Randomness.attach(rand_address)
+  }
+
+  return randomness
+}
+
+setLottery = async (lottery, memeNft, randomness) => {
+  await memeNft.setLotteryContract(lottery)
+  await randomness.setLotteryAddress(lottery)
+} 
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,22 +81,12 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   //await hre.run('compile');
+  
   const deployer = await ethers.getSigner();
-  const MemeToken = await hre.ethers.getContractFactory("MemeXToken");
-  const token = await MemeToken.deploy("MEMEX", "MemeX", 1000000, deployer.address);
-  await token.deployed();
-  console.log("Token deployed to:", token.address);
-
-  const Staking = await hre.ethers.getContractFactory("MemeXStaking");
-  const stake = await Staking.deploy(token.address, deployer.address);
-  await stake.deployed();
-  console.log("Staking deployed to:", stake.address);
-
-  const Lottery = await hre.ethers.getContractFactory("Lottery");
-  const lottery = await Lottery.deploy(stake.address);
-  await lottery.deployed();
-  console.log("Lottery deployed to:", lottery.address);
-
+  token = await deploy_MemeXToken(deployer)
+  stake = await deploy_Staking(deployer,token)
+  lottery = await deploy_Lottery(deployer)
+  randomness = await deploy_Randomness()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
