59fd967efe5148be986226f171d5d5a28b04721a
sssubik <subikshrestha@gmail.com>
2021-09-07 15:01:23 +0545

    adding access control to ERC1155Factory

--- contracts/Factory/ERC1155Factory.sol
+++ contracts/Factory/ERC1155Factory.sol
@@ -3,22 +3,39 @@ pragma solidity ^0.8.0;
 import "../ERC1155/MemeXNFT.sol";
 import "../Utils/CloneFactory.sol";
 import "../../interfaces/IMemeXNFT.sol";
-contract MemeXNFTFactory is CloneFactory{
+import "../Access/MemeXAccessControls.sol";
+
+contract MemeXNFTFactory is CloneFactory, MemeXAccessControls{
 //TODO TEMPLATE ID
 //TODO : ACCESS CONTROL
     bool public locked;
 
     uint256 public MemeXNFTTemplateId;
     mapping(uint256 => address) private MemeXNFTTemplates;
+    mapping(address => MemeXNFT) public MemeXNFTInfo;
+    address[] public MemeXNFTs;
+    mapping(address => uint256) private templateToId;
+    
 
     struct MemeXNFT {
         bool exists;
+        uint256 templateId;
         uint256 index;
     }
 
-    mapping(address => MemeXNFT) public memeXInfo;
-    address[] memeXs;
+    event NFTCreated(address indexed owner, address indexed addr, address template);
+    event TemplateAdded(address template, uint256 templateId);
+    event TemplateRemoved(address template, uint256 templateId);
 
+    constructor(){
+        locked = true;
+    }
+
+    function setLocked(bool _locked) public{
+        require(hasOperatorRole(msg.sender)
+                 || hasAdminRole(msg.sender),"setLocked: Must be operator or admin");
+        locked = _locked;
+    }
     function deployMemeXNFT(
         string memory _name,
         string memory _symbol,
@@ -26,11 +43,50 @@ contract MemeXNFTFactory is CloneFactory{
         string memory _baseUri,
         uint256 _templateId
     ) public returns (address NFT) {
-        NFT = createClone(MemeXNFTTemplates[1]);
+        if (locked){
+            require(hasOperatorRole(msg.sender)
+                 || hasAdminRole(msg.sender),"deployMemeXNFT: Must be operator or admin");
+        }
+        require(MemeXNFTTemplates[_templateId] != address(0));
+        NFT = createClone(MemeXNFTTemplates[_templateId]);
         IMemeXNFT(NFT).initNFT(_name,_symbol,_lotteryContract);
+        MemeXNFTInfo[NFT] = MemeXNFT(true, _templateId, MemeXNFTs.length);
+        MemeXNFTs.push(NFT);
+        emit NFTCreated(msg.sender, address(NFT),MemeXNFTTemplates[_templateId]);
     }
 
+
     function addMemeXNFTTemplate(address _template) public{
-        MemeXNFTTemplates[1] = _template;
+        //Add Access Control
+        require(hasOperatorRole(msg.sender)
+                 || hasAdminRole(msg.sender),"addMemeXNFTemplate: Must be operator or admin");
+        require(templateToId[_template] ==0);
+        MemeXNFTTemplateId += 1;
+        MemeXNFTTemplates[MemeXNFTTemplateId] = _template;
+        templateToId[_template] = MemeXNFTTemplateId;
+        emit TemplateAdded(_template, MemeXNFTTemplateId);
+    }
+
+    function removeMemeXNFTTemplate(uint256 _templateId) public {
+        //Add access control
+        require(hasOperatorRole(msg.sender)
+                 || hasAdminRole(msg.sender),"removeMemeXNFTTemplate: Must be operator or admin");
+        require(MemeXNFTTemplates[_templateId] != address(0));
+        address template = MemeXNFTTemplates[_templateId];
+        MemeXNFTTemplates[_templateId] = address(0);
+        delete templateToId[template];
+        emit TemplateRemoved(template, _templateId);
+    }
+
+    function numberOfNFTs() external view returns (uint256) {
+        return MemeXNFTs.length;
+    }
+
+    function getTemplate(uint256 _templateId) external view returns (address) {
+        return MemeXNFTTemplates[_templateId];
+    }
+
+    function getTemplateId(address _template) external view returns (uint256) {
+        return templateToId[_template];
     }
 }
