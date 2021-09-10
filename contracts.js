86b07835552fcbb830ef2624a9565a15fc533a2b
Dante <dante.dpf@gmail.com>
2021-09-10 17:57:04 -0300

    Creating soft staking contract

--- contracts.js
+++ contracts.js
@@ -1,12 +1,11 @@
 CONTRACTS = {
     "rinkeby": {
-        "lotteryAddress": "0xe49ee16Afe4853c2C14F7a9CBe5B7BD6Da15E980",
+        "lotteryAddress": "0x061d6Ffca8Fd3E345a6C188DCeBAe889465A1C23",
         "tokenAddress": "0xefD5C859ae4c569177aafEF578eEC99885460146",
         "pinaAddress": "0x2Da76A63d8b6D51dc29141524A18E0c28c74C340",
         "nftAddress": "0x8410645aeD27109c10e60088C1fD939aFeAE7203",
         "randomnessAddress": "0x53245f76B812b115764C26F1BA1B8CC02695834C",
-        "stakeTokenAddress": "0xCf27592806980801a8B7d2Ed9283A18F80968DA3",
-        "stakeLiquidityAddress": ""
+        "softStakeAddress": "0x0480B26aCF8AcE1524bbC21B53346E9E0FA813f1",
     },
     "localhost": {
         "lotteryAddress": "",
