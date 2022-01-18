70bebefaf4f44f232eecc4e713383eb66e06f6e7
fornax <dante.dpf@gmail.com>
2022-01-18 18:18:36 -0300

    updating addresses

--- contracts.js
+++ contracts.js
@@ -2,20 +2,18 @@ CONTRACTS = {
     "rinkeby": {
         "chainId": "",
         "lotteryAddress": "",
-        "tokenAddress": "0x65cD770B9FA597F11f4665746A10e19294aFF65f",
         "nftAddress": "0x9d02Ac9230516aa4FA820b0D3A4DEaf89bED9Adc",
         "randomnessAddress": "0xB988eF0C42B682E59b0215b08a985a0d4C3225A9",
         "rewardsAddress": "0x5ea6D0aDf94D315B6E1160d2E9D3fb6A2E1Bc0bF",
     },
     "fantomtestnet": {
-        "lotteryAddress": "0x8c54C597e15A13677C0C3EBB0fC217443268830a",
-        "nftAddress": "0x7f5a5672471C2dDa79d9d002753f0fdE9906EfAf",
+        "lotteryAddress": "0x9935C818C29ab7068Ac1bd8Aa5e878fE09cf412e",
+        "nftAddress": "0xe168a9697808F865F47f200CbBEF085c492e598f",
         "randomnessAddress": "0x0704e09cec2727De730Fd74B8Cf39459587ab39B",
-        "rewardsAddress": "0x7C79E15dFEB6887142F5F6968E2717B4DFc7ED15",
+        "rewardsAddress": "0x78f6E042131EE4aA861b1B83D5f9460da4aB6169",
     },
     "fantom": {
         "lotteryAddress": "",
-        "tokenAddress": "",
         "nftAddress": "",
         "randomnessAddress": "",
         "rewardsAddress": "",
