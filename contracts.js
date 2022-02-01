0a687ca0e4aaf3ddf4c830f611d9200a5c39a865
fornax <dante.dpf@gmail.com>
2022-02-01 16:16:22 -0300

    new addresses

--- contracts.js
+++ contracts.js
@@ -8,15 +8,17 @@ CONTRACTS = {
     },
     "fantomtestnet": {
         "lotteryAddress": "0x83091D75D4d722F9E4644bbcC654E40c4E484538",
-        "nftAddress": "0xd003A139D8Ac0F81D0CE5a1C5D43E310D565EE58",
+        "nftAddress": "0x4941E85CB04A1A881a34173556f95EDdB0395416",
         "randomnessAddress": "0x6442D7Eb2e83dcFF36C0117D3184B4ABD14a4e96",
         "rewardsAddress": "0x2fb37Be90035E8341C593FF6e827155C2aDB737A",
+        "auctionAddress": "0x564964CeAA9c16d4105669b01d1AB1d6B50e2a68",
     },
     "fantom": {
         "lotteryAddress": "0x62F3ec0de5053D88744C91Db11a4b659F8FbbF71",
         "nftAddress": "0x3Ccf4eaC07E4186E7a2BF3Ab0B2C744ADB87939A",
         "randomnessAddress": "0x858Ed9AD6056E1B6466C3B5EeBb52d621415E127",
         "rewardsAddress": "0x6B6d43406C700582a91dAB59d8195608F0b136e9",
+        "auctionAddress": "",
     }
 }
 
