f44ade60c3d5d8b12f8c374f58a161b0483213ad
Fornax <23104993+fornax2@users.noreply.github.com>
2022-03-12 09:54:15 -0300

    typescript migration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-truffle4");
-require("hardhat-gas-reporter");
-require("@nomiclabs/hardhat-solhint");
-require("@typechain/hardhat");
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-require("@openzeppelin/hardhat-upgrades");
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomiclabs/hardhat-solhint"
+import "@nomiclabs/hardhat-ethers"
+import "@openzeppelin/hardhat-upgrades"
+
+dotenv.config();
+
+
+//require("@nomiclabs/hardhat-truffle4");
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -19,26 +25,28 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  networks: {
+ const config: HardhatUserConfig = { 
+  networks: {  
     rinkeby: {
       url: process.env.PROVIDER_URL,
-      accounts: [process.env.DEPLOYER_PK],
+      accounts:
+      process.env.DEPLOYER_PK !== undefined ? [process.env.DEPLOYER_PK] : [],
     },
     fantomtestnet: {
       url: process.env.PROVIDER_URL,
-      accounts: [process.env.DEPLOYER_PK],
+      accounts:
+      process.env.DEPLOYER_PK !== undefined ? [process.env.DEPLOYER_PK] : [],
       chainId: 0xfa2,
     },
     fantom: {
       url: process.env.PROVIDER_URL,
-      accounts: [process.env.DEPLOYER_PK],
+      accounts:
+      process.env.DEPLOYER_PK !== undefined ? [process.env.DEPLOYER_PK] : [],
       chainId: 0xfa,
     },
     hardhat: {
       gas: 12000000,
       allowUnlimitedContractSize: false,
-      timeout: 1800000,
       accounts: {
         count: 100,
       },
@@ -69,3 +77,5 @@ module.exports = {
     coinmarketcap: `${process.env.COINMARKETCAP_KEY}`,
   },
 };
+
+export default config;
