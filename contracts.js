c29f2ac3bee60d8f0eb5e477ba243289ed7062e1
Fornax <23104993+fornax2@users.noreply.github.com>
2022-05-02 14:08:28 -0300

    new addresses

--- contracts.js
+++ contracts.js
@@ -15,11 +15,11 @@ CONTRACTS = {
         "currentMembershipAddress": "0x0000000000000000000000000000000000000000",
     },
     "dev": {
-        "lotteryAddress": "0x4563e9F515B98084E44b676554723939344Cd392",
-        "nftAddress": "0xD5d21823AFd1f5a15bA4E032E4f452170b97703C",
-        "randomnessAddress": "0x9b3D01F0B013e1d25B645A00938E64614F30b8a6",
-        "rewardsAddress": "0x395FcDFd955737a5b2f0eb34A38cD085134aCf44",
-        "auctionAddress": "0x4f7119CEA8078533933C52704BfB3dE27C6bc6b1",
+        "lotteryAddress": "0x288601B1972C32A99F6284a6d2AB44a3e72e54A3",
+        "nftAddress": "0x5CBaEd483ed8c09D659e7F5172A3024e0a6A9CA1",
+        "randomnessAddress": "0xe827372E205234c3324eaA6079ee1F71F41f4210",
+        "rewardsAddress": "0x7674bCE109c482ae4b80Ef3E86C1093C3b403105",
+        "auctionAddress": "0x93824fF6f4bD1c73cb5Ad3849FB4b60c86aF28F9",
         "currentMembershipAddress": "0x0000000000000000000000000000000000000000",
     },
     "fantom": {
