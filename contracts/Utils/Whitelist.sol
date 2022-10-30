e5608a3b801a9c0765edeb33620ed1dbf4950b58
fornax2 <23104993+fornax2@users.noreply.github.com>
2022-10-30 15:27:32 -0300

    whitelists

--- contracts/Utils/Whitelist.sol
+++ contracts/Utils/Whitelist.sol
@@ -1,37 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../interfaces/IWhitelist.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-
 interface IBalanceOf {
     function balanceOf(address owner) external view returns (uint256 balance);
 }
 
-contract Whitelist is AccessControl, IWhitelist {
-    mapping(uint256 => WhitelistTarget[]) public whitelist;
+contract WhitelistAlpha {
+    address constant ALPHA = 0x48AF7b1c9dac8871C064f62FcEC0d9d6F7c269f5;
 
-    struct WhitelistTarget {
-        // user must have a balance on contractAddress of at least minBalance to be whitelisted
-        address contractAddress;
-        uint256 minBalance;
-    }
-
-    constructor(address _admin) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _admin);
-    }
-
-    /**
-     * @dev Throws if not called by an admin account.
-     */
-    modifier onlyAdmin() {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Admin calls only");
-        _;
-    }
+    constructor() {}
 
     /**
      * @notice Assess whether an address meets requirements to be considered whitelisted
-     * Will check if the address contains at least minBalance tokens on any of the stored contract addresses.
+     * Will check if the address has the target token balance.
      * @param _address The address to assess whitelist status.
      * @return True if the address is whitelisted, false otherwise.
      */
@@ -40,54 +21,6 @@ contract Whitelist is AccessControl, IWhitelist {
         view
         returns (bool)
     {
-        WhitelistTarget[] memory targets = whitelist[_collectionId];
-        for (uint256 i = 0; i < targets.length; i++) {
-            WhitelistTarget memory target = targets[i];
-            if (
-                // works for ERC-20 or ERC-721 tokens
-                IBalanceOf(target.contractAddress).balanceOf(_address) >=
-                target.minBalance
-            ) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * @notice Add a new contract address (ERC-20 or ERC-721) and minBalance.
-     * If an address has at least minBalance tokens on any added contract it will be whitelisted.
-     * @param _address, ERC-20/ERC-721 contract address
-     * @param _minBalance mininum balance to be considered whitelisted
-     * @param _collectionId collection id for which the requirements will be applied
-     */
-    function addAddress(
-        address _address,
-        uint256 _minBalance,
-        uint256 _collectionId
-    ) public onlyAdmin {
-        require(_minBalance > 0, "Min balance must be greater than 0");
-        WhitelistTarget memory param = WhitelistTarget(_address, _minBalance);
-        whitelist[_collectionId].push(param);
-    }
-
-    /**
-     * @notice remove a contract address from the requirements list
-     * @param _address contract address to be removed
-     * @param _collectionId collection id for which the requirements apply
-     */
-    function removeAddress(address _address, uint256 _collectionId)
-        public
-        onlyAdmin
-    {
-        WhitelistTarget[] storage list = whitelist[_collectionId];
-
-        for (uint256 i = 0; i < list.length; i++) {
-            if (list[i].contractAddress == _address) {
-                list[i] = list[list.length - 1];
-                list.pop();
-                return;
-            }
-        }
+        return (IBalanceOf(ALPHA).balanceOf(_address) > 0);
     }
 }
